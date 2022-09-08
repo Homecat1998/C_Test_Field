75c3554472cb293a941307761ef9f3fb511f3205
Zhetao <u6211344@anu.edu.au>
2022-09-08 19:40:10 +0800

    first OO try

---
+++ helloworld.cpp
@@ -0,0 +1,91 @@
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Box
+{
+public:
+    double getLength();
+    double getWidth();
+    double getHeight();
+    void setBox(double &len, double &wid, double &hei);
+
+    Box();
+    Box(double len, double wid, double hei);
+    ~Box();
+
+private:
+    double length; // 盒子的长度
+    double width;  // 盒子的宽度
+    double height; // 盒子的高度
+};
+
+// constructor for empty box
+Box::Box()
+{
+    cout << "An empty box has been created." << endl;
+    width = 0.0f;
+    height = 0.0f;
+    length = 0.0f;
+}
+
+// constructor for a box with parameters
+Box::Box(double len, double wid, double hei)
+{
+
+    cout << "An box has been created." << endl;
+    setBox(len, wid, hei);
+}
+
+// destructor
+Box::~Box()
+{
+    cout << "The box has been destroyed." << endl;
+}
+
+double Box::getLength()
+{
+    return length;
+}
+
+double Box::getWidth()
+{
+    return width;
+}
+
+double Box::getHeight()
+{
+    return height;
+}
+
+void Box::setBox(double &len, double &wid, double &hei)
+{
+    length = len;
+    width = wid;
+    height = hei;
+    cout << "An box has been set." << endl;
+    cout << "Length: " << length << endl;
+    cout << "Width: " << width << endl;
+    cout << "Height: " << height << endl;
+}
+
+int main()
+{
+
+    Box box1(1.0, 1.0, 1.0);
+    Box box2;
+
+    double b2_len = 2.0;
+    double b2_wid = 2.0;
+    double b2_hei = 2.0;
+    box2.setBox(b2_len, b2_wid, b2_hei);
+
+
+
+
+
+}
